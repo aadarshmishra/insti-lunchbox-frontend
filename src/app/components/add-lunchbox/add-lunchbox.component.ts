44489eef77fae62eb122bf16912c985626be684f
Aadarsh Mishra <aadarsh9600@gmail.com>
2022-05-07 22:27:01 +0530

    Added Lunchbox func.

---
+++ src/app/components/add-lunchbox/add-lunchbox.component.ts
@@ -0,0 +1,61 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Fooditem } from 'src/app/interfaces/fooditem';
+import { Institute } from 'src/app/interfaces/Institute';
+import { Lunchbox } from 'src/app/interfaces/lunchbox';
+import { FooditemService } from 'src/app/services/fooditem.service';
+import { InstituteService } from 'src/app/services/institute.service';
+import { LunchboxService } from 'src/app/services/lunchbox.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-add-lunchbox',
+  templateUrl: './add-lunchbox.component.html',
+  styleUrls: ['./add-lunchbox.component.scss']
+})
+export class AddLunchboxComponent implements OnInit {
+
+  constructor(private fooditemService: FooditemService,
+    private instituteService: InstituteService,
+    private lunchboxService: LunchboxService,
+    private router: Router) { }
+
+  fooditem: Fooditem = {} as Fooditem;
+  lunchbox: Lunchbox = {} as Lunchbox;
+  
+  ngOnInit(): void {
+  }
+
+  public goToDashboard() {
+    this.router.navigateByUrl('insti-dashboard');
+  }
+  
+  public addFooditem(addFooditemForm: NgForm) {
+    this.fooditem.fname = addFooditemForm.value.fname;
+    this.fooditem.fqty = addFooditemForm.value.fqty;
+    this.fooditem.fremark = addFooditemForm.value.fremark;
+    
+    this.instituteService.getInstituteByEmail("a@gmail.com").subscribe({
+      next: (response: Institute) => {
+        console.log(response);
+        this.lunchbox.institute = response;
+        this.fooditem.lunchbox = this.lunchbox;
+        this.lunchbox.institute = response;
+        console.log(this.fooditem);
+        this.fooditemService.addFoodItem(this.fooditem).subscribe(
+          (response: Fooditem) => {
+            console.log(response);
+            addFooditemForm.reset();
+          },
+          (error: HttpErrorResponse) => {
+            alert(error.error.message);
+          }
+        );
+      },
+      error: (error: HttpErrorResponse) => {
+      }
+    })
+  }
+}
